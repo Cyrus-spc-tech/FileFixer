b9c612f01af25a8d02816f44b85bb29ce142cf8b
Tanish Gupta <tanishgupta12389@gmail.com>
2025-09-01 23:26:22 +0530

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="filefix",
-    author="",
-    version="0.1.1",
+    author="cyrus",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         'typer',
